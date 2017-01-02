1173df180183b4e79f69a71483d4d30196488089
macaba <macaba@gmail.com>
2017-01-02 17:44:09 +0000

    Fixed empty field in assembly info

--- OSHParkTool/Properties/AssemblyInfo.cs
+++ OSHParkTool/Properties/AssemblyInfo.cs
@@ -10,6 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("OSHParkTool")]
+[assembly: AssemblyCopyright("Copyright © Macaba 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
