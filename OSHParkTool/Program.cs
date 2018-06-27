d36ec2be2b8f0f326e726afdf3ca3a30c38c0cd5
macaba <macaba@gmail.com>
2018-06-27 22:52:30 +0100

    Added support for stencil zip

--- OSHParkTool/Program.cs
+++ OSHParkTool/Program.cs
@@ -15,6 +15,26 @@ namespace OSHParkTool
             AppDomain currentDomain = AppDomain.CurrentDomain;
             currentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
 
+            CreateMainFile();
+            CreateStencilFile();
+            
+            Console.WriteLine("Press any key to exit.");
+            Console.Read();
+            
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception ex = (Exception)e.ExceptionObject;
+            Console.WriteLine("Exception caught : " + ex.Message);
+            //Console.WriteLine("Runtime terminating: {0}", e.IsTerminating);
+            Console.WriteLine("Press any key to exit.");
+            Console.Read();
+            Environment.Exit(0);
+        }
+
+        private static void CreateMainFile()
+        {
             Dictionary<string, string> rules = new Dictionary<string, string>();
             rules.Add("CADCAM Top Copper.TXT", ".GTL");
             rules.Add("CADCAM Bottom Copper.TXT", ".GBL");
@@ -62,7 +82,7 @@ namespace OSHParkTool
             if (count > 0)
             {
                 int i = 0;
-                using (ZipArchive zip = ZipFile.Open(Path.Combine(directory, "OSH Park.zip"), ZipArchiveMode.Create))
+                using (ZipArchive zip = ZipFile.Open(Path.Combine(directory, "Gerbers.zip"), ZipArchiveMode.Create))
                 {
                     foreach (string file in files)
                     {
@@ -71,7 +91,7 @@ namespace OSHParkTool
                             string key = rules.Keys.First(r => file.Contains(r));
                             string extension = rules[key];
                             string newFile = Path.ChangeExtension(file, extension);
-                            
+
                             //File.Move(file, newFile);
                             zip.CreateEntryFromFile(file, Path.GetFileName(newFile));
                             Console.WriteLine("Added file to zip: " + Path.GetFileName(newFile));
@@ -79,24 +99,62 @@ namespace OSHParkTool
                         }
                     }
                 }
-                
-                    
-                
-
             }
-            Console.WriteLine("Press any key to exit.");
-            Console.Read();
-            
         }
 
-        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        private static void CreateStencilFile()
         {
-            Exception ex = (Exception)e.ExceptionObject;
-            Console.WriteLine("Exception caught : " + ex.Message);
-            //Console.WriteLine("Runtime terminating: {0}", e.IsTerminating);
-            Console.WriteLine("Press any key to exit.");
-            Console.Read();
-            Environment.Exit(0);
+            Dictionary<string, string> rules = new Dictionary<string, string>();
+
+            rules.Add("CADCAM Mechanical 1.TXT", ".GKO");     //Board outline
+            rules.Add("CADCAM Mechanical 2.TXT", ".GKO");     //Board outline
+            rules.Add("CADCAM Mechanical 3.TXT", ".GKO");     //Board outline
+            rules.Add("CADCAM Mechanical 4.TXT", ".GKO");     //Board outline
+            rules.Add("CADCAM Drill.TXT", ".XLN");
+
+            //New proteus
+            rules.Add("CADCAM Top SMT Paste.GBR", ".GBR");
+            rules.Add("CADCAM Bottom SMT Paste.GBR", ".GBR");
+            rules.Add("CADCAM Mechanical 1.GBR", ".GKO");     //Board outline
+            rules.Add("CADCAM Mechanical 2.GBR", ".GKO");     //Board outline
+            rules.Add("CADCAM Mechanical 3.GBR", ".GKO");     //Board outline
+            rules.Add("CADCAM Mechanical 4.GBR", ".GKO");     //Board outline
+
+            string directory = new Uri(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase)).LocalPath;
+            Console.WriteLine("Source directory: " + directory);
+            string[] files = Directory.GetFiles(directory);
+            int count = 0;
+            foreach (string file in files)
+                if (rules.Keys.Any(r => file.Contains(r)))
+                    count++;
+
+            Console.WriteLine("Found " + count.ToString() + " applicable files in source directory.");
+
+            if (count > 0)
+            {
+                int i = 0;
+                using (ZipArchive zip = ZipFile.Open(Path.Combine(directory, "Stencil.zip"), ZipArchiveMode.Create))
+                {
+                    foreach (string file in files)
+                    {
+                        if (rules.Keys.Any(r => file.Contains(r)))
+                        {
+                            string key = rules.Keys.First(r => file.Contains(r));
+                            string extension = rules[key];
+                            string newFile = Path.ChangeExtension(file, extension);
+
+                            //File.Move(file, newFile);
+                            zip.CreateEntryFromFile(file, Path.GetFileName(newFile));
+                            Console.WriteLine("Added file to zip: " + Path.GetFileName(newFile));
+                            i++;
+                        }
+                    }
+                }
+
+
+
+
+            }
         }
     }
 }
